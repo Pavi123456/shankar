f325f88046eb0a793685e1935a197fd771f83e1e
santhoshshankar <31506543+santhoshshankar@users.noreply.github.com>
2018-02-13 14:16:52 +0530

    Create n no.c

---
+++ n no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main( )
+{  
+    int num, temp;  
+    long productOfDigit = 1;  
+    printf("Enter a Number\n");  
+    scanf("%d", &num);  
+    temp = num;
+    while(num != 0){ 
+        productOfDigit *= num % 10;
+        num = num/10;
+    } 
+    printf("Product of digits of %d = %ld", temp, productOfDigit);  
+    getch();
+    return 0;  
+    }
